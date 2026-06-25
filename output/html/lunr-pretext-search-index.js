72c15bb3bcd3ce57fa6a8a4a2db1f638395f14ef
fflorit <federico.florit@gmail.com>
2026-06-25 18:57:20 +0200

    Aggiunta capitolo insiemi e inizio scrittura, pulizia file, modifiche minori

--- output/html/lunr-pretext-search-index.js
+++ output/html/lunr-pretext-search-index.js
@@ -19,11 +19,56 @@ var ptx_lunr_docs = [
   "body": " Prefazione...  "
 },
 {
+  "id": "sec-insiemi",
+  "level": "1",
+  "url": "sec-insiemi.html",
+  "type": "Paragrafo",
+  "number": "1.1",
+  "title": "Insiemi, intervalli, operazioni tra insiemi",
+  "body": " Insiemi, intervalli, operazioni tra insiemi   Gli insiemi e i sottoinsiemi sono alla base della maggior parte delle scritture matematiche. Tra i più usati possiamo ricordare gli insiemi numerici, l'insieme delle soluzioni di un'equazione o una disequazione, l'insieme di definizione di una funzione, eccetera. Gli insiemi in generale non contengono solo numeri: potrebbero contenere oggetti astratti, parole, lettere e perfino altri insiemi.    Insiemi e operazioni  La definizione di insieme non esiste, perchè è un ente primitivo della matematica e non ci serve una definizione per capire cos'è in quanto è intuitivo. Definiamo quindi solo la notazione: un insieme composto si indica solitamente con una lettera maiuscola e i suoi elementi, se indicati singolarmente, vanno indicati dopo l'uguale, tra parentesi graffe separati da virgole Per esempio: l'insieme delle lettere che compongono la parola matematica è ; l'insieme delle soluzioni (radici) dell'equazione di secondo grado è o anche . Per indicare che un elemento appartiene a un insieme (cioè è uno degli elementi dell'insieme) useremo il simbolo . Negli esempi precedenti (tra gli altri) e . Se un elemento non appartine a un insieme usiamo il simbolo . Per esempio la lettera b non fa parte della parola matematica , quindi .  Se un insieme non contine elementi si chiama insieme vuoto e si indica con il simbolo . Per esempio: l'equazione non ammette soluzioni reali e quindi diremo che l'insieme delle soluzioni è vuoto . Attenzione!  non è la stessa cosa: in questo caso indica l'insieme che contiene l'insieme vuoto, quindi non è vuoto, ma contiene un elemento!  Tra insiemi si posso fare delle operazioni binarie (cioè fra due insiemi e ) che danno come risultato un altro insieme e in particolare utilizzeremo:  l' unione  , cioè l'insieme gli elementi comuni e non comuni dei due insiemi;  l' intersezione  , cioè l'insieme dei soli elementi comuni dei due insiemi  Per esempio consideriamo le parole matematica e divertente e gli insiemi e delle lettere che compongono le due parole. Le parole matematica divertente hanno come insieme delle lettere l'unione dei due insiemi (da notare che non abbiamo contato due volte gli elementi comuni). L'insieme delle lettere che accomunano le due parole è invece l'intersezione dei due insiemi di partenza (quindi c'è qualcosa di divertente nella matematica e viceversa!). Se avessimo scelto altre parole l'intersezione avrebbe potuto dare come risultato l'insieme vuoto: radici e bello non hanno lettere in comune, infatti e hanno (non c'è niente di bello nelle radici e viceversa!). Per questi esempi unione e intersezione vanno fatte confrontando gli insiemi manualmente; vedremo che per gli insiemi numerici queste operazioni sono molto più veloci.    Insiemi numerici  Gli insiemi che contengono solo numeri sono chiamati insiemi numerici . Tra i sottoinsiemi degli insiemi numerici troviamo anche gli intervalli .   "
+},
+{
+  "id": "sec-insiemi-3-2",
+  "level": "2",
+  "url": "sec-insiemi.html#sec-insiemi-3-2",
+  "type": "Capoverso (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "insieme appartiene non appartine "
+},
+{
+  "id": "sec-insiemi-3-3",
+  "level": "2",
+  "url": "sec-insiemi.html#sec-insiemi-3-3",
+  "type": "Capoverso (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "insieme vuoto "
+},
+{
+  "id": "sec-insiemi-3-4",
+  "level": "2",
+  "url": "sec-insiemi.html#sec-insiemi-3-4",
+  "type": "Capoverso (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "unione intersezione "
+},
+{
+  "id": "sec-insiemi-4-2",
+  "level": "2",
+  "url": "sec-insiemi.html#sec-insiemi-4-2",
+  "type": "Capoverso (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "insiemi numerici intervalli "
+},
+{
   "id": "ch-equazioni",
   "level": "1",
   "url": "ch-equazioni.html",
   "type": "Capitolo",
-  "number": "1",
+  "number": "2",
   "title": "Equazioni",
   "body": " Equazioni   TODO   "
 },
@@ -32,7 +77,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-disequazioni.html",
   "type": "Capitolo",
-  "number": "2",
+  "number": "3",
   "title": "Disequazioni",
   "body": " Disequazioni   TODO   "
 },
@@ -41,7 +86,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-funzioni.html",
   "type": "Capitolo",
-  "number": "3",
+  "number": "4",
   "title": "Funzioni",
   "body": " Funzioni   TODO   "
 },
@@ -50,7 +95,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-piano-cartesiano.html",
   "type": "Capitolo",
-  "number": "4",
+  "number": "5",
   "title": "Piano cartesiano",
   "body": " Piano cartesiano   TODO   "
 },
@@ -59,7 +104,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-equazione.html",
   "type": "Paragrafo",
-  "number": "5.1",
+  "number": "6.1",
   "title": "Equazione della retta",
   "body": " Equazione della retta   Intro to section, because we will have subsections.  Another paragraph of the introduction.    Teoremi e dimostrazioni  Content of the subsection.  More content.   Equazione della retta passante per l'origine   Le rette che passano per l'origine hanno equazione esplicita con .      Dimostrazione ...       After the last subsection, you might have a conclusion.   "
 },
@@ -68,7 +113,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-equazione.html#thm-derivazione_retta",
   "type": "Teorema",
-  "number": "5.1.1",
+  "number": "6.1.1",
   "title": "Equazione della retta passante per l’origine.",
   "body": " Equazione della retta passante per l'origine   Le rette che passano per l'origine hanno equazione esplicita con .      Dimostrazione ...    "
 },
@@ -77,7 +122,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-retta_interattiva.html",
   "type": "Paragrafo",
-  "number": "5.2",
+  "number": "6.2",
   "title": "Esempio interattivo",
   "body": " Esempio interattivo    Effetto di e sul grafico di una retta.    "
 },
@@ -86,7 +131,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-retta_interattiva.html#sec-retta_interattiva-2",
   "type": "Figura",
-  "number": "5.2.1",
+  "number": "6.2.1",
   "title": "",
   "body": " Effetto di e sul grafico di una retta.   "
 },
@@ -95,7 +140,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-parabola_interattiva.html",
   "type": "Paragrafo",
-  "number": "5.3",
+  "number": "6.3",
   "title": "Esempio interattivo",
   "body": " Esempio interattivo    Effetto di e sul grafico di una retta.    "
 },
@@ -104,7 +149,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-parabola_interattiva.html#sec-parabola_interattiva-2",
   "type": "Figura",
-  "number": "5.3.1",
+  "number": "6.3.1",
   "title": "",
   "body": " Effetto di e sul grafico di una retta.   "
 },
@@ -113,7 +158,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-parabola.html",
   "type": "Capitolo",
-  "number": "6",
+  "number": "7",
   "title": "Parabola",
   "body": " Parabola   TODO   "
 },
@@ -122,7 +167,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-esponenziali-logaritmi.html",
   "type": "Capitolo",
-  "number": "7",
+  "number": "8",
   "title": "Funzioni esponenziali e logaritmiche",
   "body": " Funzioni esponenziali e logaritmiche   TODO   "
 },
@@ -131,7 +176,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-limiti.html",
   "type": "Capitolo",
-  "number": "8",
+  "number": "9",
   "title": "Limiti",
   "body": " Limiti   TODO   "
 },
@@ -140,7 +185,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-derivate.html",
   "type": "Capitolo",
-  "number": "9",
+  "number": "10",
   "title": "Derivate",
   "body": " Derivate   TODO   "
 },
@@ -149,7 +194,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-sudio-funzione.html",
   "type": "Capitolo",
-  "number": "10",
+  "number": "11",
   "title": "Studio di funzione",
   "body": " Studio di funzione   TODO   "
 },
@@ -158,7 +203,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-economia.html",
   "type": "Capitolo",
-  "number": "11",
+  "number": "12",
   "title": "Applicazioni all’economia",
   "body": " Applicazioni all'economia   TODO   "
 },
@@ -167,7 +212,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-mat-fin.html",
   "type": "Capitolo",
-  "number": "12",
+  "number": "13",
   "title": "Matematica finanziaria",
   "body": " Matematica finanziaria   TODO   "
 },
