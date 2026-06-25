857bb5dd173121129aa8d25a4bc0248fcd173641
fflorit <federico.florit@gmail.com>
2026-06-25 21:28:10 +0200

    Capitolo affermazioni matematiche, creazione del capitolo operazioni algebriche

--- output/html/lunr-pretext-search-index.js
+++ output/html/lunr-pretext-search-index.js
@@ -19,13 +19,76 @@ var ptx_lunr_docs = [
   "body": " Prefazione...  "
 },
 {
+  "id": "sec-affermazioni",
+  "level": "1",
+  "url": "sec-affermazioni.html",
+  "type": "Paragrafo",
+  "number": "1.1",
+  "title": "Affermazioni matematiche",
+  "body": " Affermazioni matematiche    You can prove anything you want by coldly logical reason — if you pick the proper postulates  Isaac Asimov, Reason in I, Robot (1950)   La matematica si basa su ragionamenti logici che partono da delle affermazioni base per trarre delle conclusioni vere finchè sono vere le affermazioni base. Ogni considerazione matematica che abbiamo visto (e che vedremo) è basata su questo principio. Elenchiamo ora come si chiamano queste affermazioni nel linguaggio della matematica.    Definizione   Si chiama definizione una proposizione che descrive, chiaramente e sinteticamente, un ente matematico (algebrico o geometrico) servendosi di termini aventi un significato noto.   La definizione è solo un chiamare qualcosa di complicato con un nome più semplice o unico . Soprattutto quando si svolgono dei conti è comodo definire con un nome diverso un insieme di oggetti\/operazioni. Ogni tanto vedremo una relazione di uguaglianza specifica ( ) per indicare che si tratta di una definizione e non di un passaggio matematico particolare.  Esempio di definizione: definizione di postulato, definizione del discriminante di un’equazione di secondo grado , definizione della variabile ausiliaria nelle equazioni risolvibili per sostituzione (come le trinomie).    Postulato o assioma   Si definisce postulato (o assioma ) un principio indimostrato la cui validità si ammette a priori per evidenza o convenzione allo scopo di fornire la spiegazione di determinati fatti o di costruire una teoria.   I postulati\/assiomi sono quindi delle affermazioni che vengono fatte senza dimostrarle, da cui si trarranno delle conclusioni. In gergo matematico si dice che un’affermazione viene postulata per dire che viene ipotizzata come vera senza dimostrazione. Gli assiomi sono alla base del metodo scientifico e spesso rappresentano le ipotesi di un ragionamento.  Esempi di assiomi: postulati di Euclide (su cui si basa la geometria classica), assiomi di Peano (su cui si basa l’algebra classica), postulato della relatività (su cui si basa la fisica), principio del terzo escluso (su cui si basa la logica proposizionale).    Teorema   Si definisce teorema un enunciato (o formula o proprietà) che può essere dimostrato, cioè che può essere dedotto logicamente dagli enunciati primitivi (assiomi\/postulati) o da altri teoremi già dimostrati.   I teoremi sono il cuore del metodo scientifico: partendo da delle ipotesi (postulate o dimostrate) si giunge in maniera logica a una tesi (l’ enunciato del teorema) Esistono molte tecniche deduttive per dimostrare un teorema, tra cui:  dimostrazione diretta: dalle ipotesi si deduce in maniera lineare la tesi attraverso uno più passaggi;  dimostrazione per assurdo (anche detta dal latino reductio ad absurdum ): si suppone che il teorema sia falso sotto le sue ipotesi e, attraverso vari passaggi, si ottiene che almeno un’ipotesi viene violata (si arriva un assurdo); ne segue che il teorema non può essere falso, quindi è vero (sulla base del principio del terzo escluso);  dimostrazione per esaustione: si dimostra il teorema per tutti i possibili casi (che non possono essere infiniti), uno a uno.   Di solito la dimostrazione di un teorema segue l’enunciato e termina con un simbolo o acronimo specifico: ■ oppure c.v.d. o CVD (Come Volevasi Dimostrare) oppure q.e.d. o QED (dal latino Quod Erat Demonstrandum ).  Esempi di teoremi: teorema di Ruffini (nella divisione tra polinomi), teorema di Weierstrass (nello studio di funzioni matematiche), teorema di Böhm-Jacopini (nella scrittura di algoritmi per l’informatica).    Corollario e lemma   Si definisce lemma una proposizione che può essere facilmente dimostrata e precede un importante teorema, così da porne le basi per la dimostrazione.    Si definisce corollario una proposizione che segue direttamente da un teorema, senza bisogno di una dimostrazione formale estesa.   Spesso i corollari sono delle conclusioni secondarie dei teoremi, nate da passaggi intermedi nella dimostrazione o da casi particolari del teorema. La differenza tra lemma, teorema e corollario è quindi solo una questione di importanza e di momento in cui una determinata proposizione è presentata in un discorso matematico.    Congettura   Si definisce congettura un enunciato che si ritiene vero ma per il quale non è stata ancora fornita una dimostrazione (per provarne la verità) o un controesempio (per dimostrarne la falsità).   Le congetture sono quindi delle affermazioni che si verificano nella pratica, senza però averne certezza logico-matematica. Sono quindi dei teoremi senza dimostrazione rigorosa.  Esempi di congetture: problemi per il millennio, congettura di Collatz.  Di solito la dimostrazione delle congetture più rilevanti porta a un premio in denaro (oltre che magari a qualche medaglia). Per esempio i problemi del millennio hanno un premio di un milione di dollari!   "
+},
+{
+  "id": "sec-affermazioni-3-2",
+  "level": "2",
+  "url": "sec-affermazioni.html#sec-affermazioni-3-2",
+  "type": "Definizione",
+  "number": "1.1.1",
+  "title": "",
+  "body": " Si chiama definizione una proposizione che descrive, chiaramente e sinteticamente, un ente matematico (algebrico o geometrico) servendosi di termini aventi un significato noto.  "
+},
+{
+  "id": "sec-affermazioni-4-2",
+  "level": "2",
+  "url": "sec-affermazioni.html#sec-affermazioni-4-2",
+  "type": "Definizione",
+  "number": "1.1.2",
+  "title": "",
+  "body": " Si definisce postulato (o assioma ) un principio indimostrato la cui validità si ammette a priori per evidenza o convenzione allo scopo di fornire la spiegazione di determinati fatti o di costruire una teoria.  "
+},
+{
+  "id": "sec-affermazioni-5-2",
+  "level": "2",
+  "url": "sec-affermazioni.html#sec-affermazioni-5-2",
+  "type": "Definizione",
+  "number": "1.1.3",
+  "title": "",
+  "body": " Si definisce teorema un enunciato (o formula o proprietà) che può essere dimostrato, cioè che può essere dedotto logicamente dagli enunciati primitivi (assiomi\/postulati) o da altri teoremi già dimostrati.  "
+},
+{
+  "id": "sec-affermazioni-6-2",
+  "level": "2",
+  "url": "sec-affermazioni.html#sec-affermazioni-6-2",
+  "type": "Definizione",
+  "number": "1.1.4",
+  "title": "",
+  "body": " Si definisce lemma una proposizione che può essere facilmente dimostrata e precede un importante teorema, così da porne le basi per la dimostrazione.  "
+},
+{
+  "id": "sec-affermazioni-6-3",
+  "level": "2",
+  "url": "sec-affermazioni.html#sec-affermazioni-6-3",
+  "type": "Definizione",
+  "number": "1.1.5",
+  "title": "",
+  "body": " Si definisce corollario una proposizione che segue direttamente da un teorema, senza bisogno di una dimostrazione formale estesa.  "
+},
+{
+  "id": "sec-affermazioni-7-2",
+  "level": "2",
+  "url": "sec-affermazioni.html#sec-affermazioni-7-2",
+  "type": "Definizione",
+  "number": "1.1.6",
+  "title": "",
+  "body": " Si definisce congettura un enunciato che si ritiene vero ma per il quale non è stata ancora fornita una dimostrazione (per provarne la verità) o un controesempio (per dimostrarne la falsità).  "
+},
+{
   "id": "sec-insiemi",
   "level": "1",
   "url": "sec-insiemi.html",
   "type": "Paragrafo",
-  "number": "1.1",
-  "title": "Insiemi, intervalli, operazioni tra insiemi",
-  "body": " Insiemi, intervalli, operazioni tra insiemi   Gli insiemi e i sottoinsiemi sono alla base della maggior parte delle scritture matematiche. Tra i più usati possiamo ricordare gli insiemi numerici, l'insieme delle soluzioni di un'equazione o una disequazione, l'insieme di definizione di una funzione, eccetera. Gli insiemi in generale non contengono solo numeri: potrebbero contenere oggetti astratti, parole, lettere e perfino altri insiemi.    Insiemi e operazioni  La definizione di insieme non esiste, perchè è un ente primitivo della matematica e non ci serve una definizione per capire cos'è in quanto è intuitivo. Definiamo quindi solo la notazione: un insieme composto si indica solitamente con una lettera maiuscola e i suoi elementi, se indicati singolarmente, vanno indicati dopo l'uguale, tra parentesi graffe separati da virgole Per esempio: l'insieme delle lettere che compongono la parola matematica è ; l'insieme delle soluzioni (radici) dell'equazione di secondo grado è o anche . Per indicare che un elemento appartiene a un insieme (cioè è uno degli elementi dell'insieme) useremo il simbolo . Negli esempi precedenti (tra gli altri) e . Se un elemento non appartine a un insieme usiamo il simbolo . Per esempio la lettera b non fa parte della parola matematica , quindi .  Se un insieme non contine elementi si chiama insieme vuoto e si indica con il simbolo . Per esempio: l'equazione non ammette soluzioni reali e quindi diremo che l'insieme delle soluzioni è vuoto . Attenzione!  non è la stessa cosa: in questo caso indica l'insieme che contiene l'insieme vuoto, quindi non è vuoto, ma contiene un elemento!  Tra insiemi si posso fare delle operazioni binarie (cioè fra due insiemi e ) che danno come risultato un altro insieme e in particolare utilizzeremo:  l' unione  , cioè l'insieme gli elementi comuni e non comuni dei due insiemi;  l' intersezione  , cioè l'insieme dei soli elementi comuni dei due insiemi  Per esempio consideriamo le parole matematica e divertente e gli insiemi e delle lettere che compongono le due parole. Le parole matematica divertente hanno come insieme delle lettere l'unione dei due insiemi (da notare che non abbiamo contato due volte gli elementi comuni). L'insieme delle lettere che accomunano le due parole è invece l'intersezione dei due insiemi di partenza (quindi c'è qualcosa di divertente nella matematica e viceversa!). Se avessimo scelto altre parole l'intersezione avrebbe potuto dare come risultato l'insieme vuoto: radici e bello non hanno lettere in comune, infatti e hanno (non c'è niente di bello nelle radici e viceversa!). Per questi esempi unione e intersezione vanno fatte confrontando gli insiemi manualmente; vedremo che per gli insiemi numerici queste operazioni sono molto più veloci.    Insiemi numerici  Gli insiemi che contengono solo numeri sono chiamati insiemi numerici . Tra i sottoinsiemi degli insiemi numerici troviamo anche gli intervalli .   "
+  "number": "1.2",
+  "title": "Insiemi, operazioni tra insiemi, intervalli",
+  "body": " Insiemi, operazioni tra insiemi, intervalli   Gli insiemi e i sottoinsiemi sono alla base della maggior parte delle scritture matematiche. Tra i più usati possiamo ricordare gli insiemi numerici, l'insieme delle soluzioni di un'equazione o una disequazione, l'insieme di definizione di una funzione, eccetera. Gli insiemi in generale non contengono solo numeri: potrebbero contenere oggetti astratti, parole, lettere e perfino altri insiemi.    Insiemi e operazioni  La definizione di insieme non esiste, perchè è un ente primitivo della matematica e non ci serve una definizione per capire cos'è in quanto è intuitivo. Definiamo quindi solo la notazione: un insieme composto si indica solitamente con una lettera maiuscola e i suoi elementi, se indicati singolarmente, vanno indicati dopo l'uguale, tra parentesi graffe separati da virgole Per esempio: l'insieme delle lettere che compongono la parola matematica è ; l'insieme delle soluzioni (radici) dell'equazione di secondo grado è o anche . Per indicare che un elemento appartiene a un insieme (cioè è uno degli elementi dell'insieme) useremo il simbolo . Negli esempi precedenti (tra gli altri) e . Se un elemento non appartine a un insieme usiamo il simbolo . Per esempio la lettera b non fa parte della parola matematica , quindi .  Se un insieme non contine elementi si chiama insieme vuoto e si indica con il simbolo . Per esempio: l'equazione non ammette soluzioni reali e quindi diremo che l'insieme delle soluzioni è vuoto . Attenzione!  non è la stessa cosa: in questo caso indica l'insieme che contiene l'insieme vuoto, quindi non è vuoto, ma contiene un elemento!  Tra insiemi si posso fare delle operazioni binarie (cioè fra due insiemi e ) che danno come risultato un altro insieme e in particolare utilizzeremo:  l' unione  , cioè l'insieme gli elementi comuni e non comuni dei due insiemi;  l' intersezione  , cioè l'insieme dei soli elementi comuni dei due insiemi  Per esempio consideriamo le parole matematica e divertente e gli insiemi e delle lettere che compongono le due parole. Le parole matematica divertente hanno come insieme delle lettere l'unione dei due insiemi (da notare che non abbiamo contato due volte gli elementi comuni). L'insieme delle lettere che accomunano le due parole è invece l'intersezione dei due insiemi di partenza (quindi c'è qualcosa di divertente nella matematica e viceversa!). Se avessimo scelto altre parole l'intersezione avrebbe potuto dare come risultato l'insieme vuoto: radici e bello non hanno lettere in comune, infatti e hanno (non c'è niente di bello nelle radici e viceversa!). Per questi esempi unione e intersezione vanno fatte confrontando gli insiemi manualmente; vedremo che per gli insiemi numerici queste operazioni sono molto più veloci.    Insiemi numerici  Gli insiemi che contengono solo numeri sono chiamati insiemi numerici . Tra i sottoinsiemi degli insiemi numerici troviamo anche gli intervalli .   "
 },
 {
   "id": "sec-insiemi-3-2",
